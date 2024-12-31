518de4ddeaac1080f11b410f5958f741a47d9cde
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2024-12-31 12:49:01 +0900

    add client

---
+++ client/client.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		// handle error
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		// handle error
+	}
+	fmt.Println(status)
+
+}
